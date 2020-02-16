6e51e782452999174aed51e90de0f45ca22ef160
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2020-02-16 17:47:58 +0900

    fix: フォームから開始終了時刻を送信できるよう修正

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -69,6 +69,6 @@ class SchedulesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def schedule_params
-      params.require(:schedule).permit(:title, :memo)
+      params.require(:schedule).permit(:title, :memo, :starttime, :endtime)
     end
 end
