91884262b2dbb003ea83618568c4eea1fd685a70
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2020-02-16 17:03:32 +0900

    chore: annotateをinstallしschema情報を書き出し

--- test/models/schedule_test.rb
+++ test/models/schedule_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: schedules
+#
+#  id         :bigint           not null, primary key
+#  title      :string           not null
+#  memo       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class ScheduleTest < ActiveSupport::TestCase
