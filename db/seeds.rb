8c8f5e87d074e4d5b25197d0ed19216c689efddb
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2020-02-16 19:35:20 +0900

    chore: 学習言語をseedで作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+names = %w[Python Ruby PHP HTML Go Java Scala]
+names.each do |name|
+  StudyLanguage.create!(name: name)
+end
