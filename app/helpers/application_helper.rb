89bb19d929ab6150cc82a5588a2382ddda9cc28a
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2020-02-16 18:57:00 +0900

    style: サインイン・サインアウトリンクをトップに表示

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def display_session_path
+    if user_signed_in?
+      link_to 'サインアウト', destroy_user_session_path, method: :delete, class: 'button is-text'
+    else
+      link_to 'サインイン', new_user_session_path, class: 'button is-light'
+    end
+  end
 end
