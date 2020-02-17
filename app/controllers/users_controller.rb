a9752749a91e596b1f771cc6ff23fdcfc0cdca3b
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2020-02-18 08:12:10 +0900

    fix: ログイン済みの時はログインページにアクセスできないよう制御

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,11 @@
 class UsersController < ApplicationController
+  before_action :check_user_session
+
   def login; end
+
+  private
+
+  def check_user_session
+    redirect_to root_path, notice: '既にログイン済みです' if user_signed_in?
+  end
 end
