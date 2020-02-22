a70ab2971ef008719c5a367df2ebe880cebe0c91
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2020-02-22 21:15:30 +0900

    fix: portfolioモデルにdependent: :destroyを追加記述

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -13,7 +13,7 @@
 
 class Portfolio < ApplicationRecord
   require 'open-uri'
-  has_many :portfolio_tags
+  has_many :portfolio_tags, dependent: :destroy
   has_many :tags, through: :portfolio_tags
   validates :url, presence: :true
   validates :url, format: { with: /\A#{URI.regexp(%w[http https])}\z/ }
