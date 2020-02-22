0ffe4232f7e260bfbd12b616d516dc3c80350fcd
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2020-02-22 23:11:14 +0900

    fix: ポートフォリオが紐づいているタグの見を表示するよう修正

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,7 +2,7 @@ class PortfoliosController < ApplicationController
   before_action :set_portfolio, only: :show
   def index
     @portfolios = Portfolio.includes(:tags)
-    @tags = Tag.all
+    @tags = Tag.has_portfolios
   end
 
   def show; end
