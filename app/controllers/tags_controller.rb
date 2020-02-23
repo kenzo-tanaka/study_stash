3920f0ff3ad8f093b13127d0d5c74a20d39aec77
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2020-02-23 18:30:33 +0900

    style: 関連リンク表示箇所をスタイリング&N+1問題を解消

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,7 @@
 class TagsController < ApplicationController
   def show
     @tag = Tag.includes(:portfolios).find(params[:id])
-    @portfolios = @tag.portfolios.includes(:tags)
+    @portfolios = @tag.portfolios.includes(:tags, :related_links)
     @tags = Tag.has_portfolios.reject{ |tag| tag == @tag }
   end
 end
