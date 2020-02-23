14ef9e5762654d23566159b0c9d347143721300e
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2020-02-23 18:06:22 +0900

    feat: アドミンポートフォリオフォームで動的に関連リンクフィールドを追加削除できるよう実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require('jquery')
 import 'bulma/css/bulma.min.css';
 import $ from "jquery";
 import 'cocoon';
