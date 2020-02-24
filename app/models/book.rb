d0c2b627e806ab1f834f6a367652632b55f2241d
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2020-02-24 15:42:59 +0900

    feat: Bookモデルを作成

---
+++ app/models/book.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: books
+#
+#  id          :bigint           not null, primary key
+#  title       :string
+#  description :text
+#  image_url   :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Book < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_24_021904) do
+ActiveRecord::Schema.define(version: 2020_02_24_064132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "image_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "portfolio_tags", force: :cascade do |t|
     t.bigint "portfolio_id", null: false
