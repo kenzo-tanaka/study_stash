f62ea1e4d62708b1fb8e3506c6f4dae6ceee619b
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2020-02-24 12:47:58 +0900

    fix: サービスコンセプトを修正しconfig管理に切り替え

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ gem 'omniauth-google-oauth2'
 gem 'meta-tags'
 gem 'nokogiri'
 gem 'cocoon'
+gem 'config'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,17 +17,16 @@ module ApplicationHelper
 
   def default_meta_tags
     {
-      site: '今日の積み上げ可視化サービス',
+      site:  Settings.service.concept,
       title: 'Study Stash',
       reverse: true,
       charset: 'utf-8',
-      description: '今日の積み上げを可視化しよう',
-      keywords: %w[今日の積み上げ],
+      description:  Settings.service.concept,
+      keywords: %w[今日の積み上げ ポートフォリオ],
       canonical: request.original_url,
       separator: '|',
       icon: [
         { href: image_url('favicon.png') },
-        # { href: image_url('apple_touch_icon.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/png' }
       ],
       og: {
         site_name: :site,
