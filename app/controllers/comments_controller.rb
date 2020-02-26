742eb9706b748392d481611b87f8a86b99eeebfe
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2020-02-26 17:18:28 +0900

    fix: comments#createアクションのリダイレクト先修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,14 +16,11 @@ class CommentsController < ApplicationController
   def create
     @comment = current_user.comments.new(comment_params)
 
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
-      else
-        format.html { render :new }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
+    if @comment.save
+      redirect_back(fallback_location: root_path)
+    else
+      format.html { render :new }
+      format.json { render json: @comment.errors, status: :unprocessable_entity }
     end
   end
 
