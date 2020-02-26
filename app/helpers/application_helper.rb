cef0a7a374d8e976d55a130708228b665d5188e1
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2020-02-26 21:48:56 +0900

    style: ナビバーのボタンスタイリングを修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def display_session_path
     if user_signed_in?
-      link_to 'サインアウト', destroy_user_session_path, method: :delete, class: 'button is-text'
+      link_to 'サインアウト', destroy_user_session_path, method: :delete, class: 'button'
     else
-      link_to 'はじめる', login_path, class: 'button is-text'
+      link_to 'はじめる', login_path, class: 'button is-success'
     end
   end
 
