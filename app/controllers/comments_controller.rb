1fe30e04a8f83fe606fa8b146dab205c3f869641
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2020-02-27 00:41:00 +0900

    feat: editリンクに対してcommentable_type, commentable_idをそれぞれパラメータとして付与することで再利用可能なedit.js.erbに修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -29,14 +29,12 @@ class CommentsController < ApplicationController
   # PATCH/PUT /comments/1
   # PATCH/PUT /comments/1.json
   def update
-    respond_to do |format|
-      if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @comment }
-      else
-        format.html { render :edit }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
+    if @comment.update(comment_params.merge(user_id: current_user.id))
+      flash[:notice] = 'コメントを更新しました。'
+      redirect_back(fallback_location: root_path)
+    else
+      flash[:notice] = 'コメントを更新できませんでした。'
+      redirect_back(fallback_location: root_path)
     end
   end
 
