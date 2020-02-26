08ee0ff0b96faefe0de76ff3151aea254ca3089b
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2020-02-26 11:03:09 +0900

    feat: commentsテーブルにポリモーフィック関連のカラム追加

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -2,11 +2,13 @@
 #
 # Table name: comments
 #
-#  id         :bigint           not null, primary key
-#  user_id    :bigint           not null
-#  content    :text
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id               :bigint           not null, primary key
+#  user_id          :bigint           not null
+#  content          :text
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  commentable_type :string
+#  commentable_id   :bigint
 #
 
 FactoryBot.define do
