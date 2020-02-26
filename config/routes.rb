6b6ec80443d3265dd2b5e949c4e24e4834b9d7ef
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2020-02-26 11:17:27 +0900

    feat: scaffold_controllerでcommnets_controllerとviewを作成

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :users, only: :show
   resources :schedules
+  resources :comments
   get '/dashboard' => 'schedules#dashboard'
   get '/login' => 'users#login'
   root 'schedules#index'
