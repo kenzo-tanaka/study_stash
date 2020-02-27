9a34eb79fa9bd802013ad323ce0df4040391ddad
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2020-02-27 21:16:26 +0900

    fix: tags#showページでのN+1問題を修正

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,7 @@
 class TagsController < ApplicationController
   def show
     @tag = Tag.includes(:portfolios).find(params[:id])
-    @portfolios = @tag.portfolios.includes(:tags, :related_links).latest
+    @portfolios = @tag.portfolios.includes(:tags, :related_links, comments: [:user]).latest
     @tags = Tag.has_portfolios.reject{ |tag| tag == @tag }
   end
 end
