3f1d2f7cbd0a4df369e2e2d8b67f77bec34d761e
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2020-02-28 13:09:08 +0900

    fix: book関連の不要なルーティングを削除

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -1,10 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "Books", type: :request do
+  let(:book) { create(:book) }
   describe "GET /books" do
-    it "works! (now write some real specs)" do
-      get books_path
-      expect(response).to have_http_status(200)
-    end
+    subject { get books_path }
+    it { is_expected.to eq 200 }
+  end
+
+  describe "GET #show" do
+    subject { get book_path(book) }
+    it { is_expected.to eq 200 }
   end
 end
