260063d6ffd30bc6c56d37efcef0fa814a4f587a
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2020-02-28 22:01:40 +0900

    feat: タグに書籍が関連づいているときは書籍をサイドバーに表示

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,5 +3,6 @@ class TagsController < ApplicationController
     @tag = Tag.includes(:portfolios).find(params[:id])
     @portfolios = @tag.portfolios.includes(:tags, :related_links, comments: [:user]).latest
     @tags = Tag.has_portfolios.reject{ |tag| tag == @tag }
+    @books = @tag.books.take(3)
   end
 end
