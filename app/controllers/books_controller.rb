455c41083ab8f4c2d7f3c4f30b74f22ee6b0cbb0
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2020-02-28 17:36:31 +0900

    feat: 関連書籍をbooks#showページに表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,6 +11,8 @@ class BooksController < ApplicationController
   # GET /books/1.json
   def show
     @comments = @book.comments.includes(:user)
+    @tags = @book.tags.includes(books: [:book_tags])
+    @related_books = @tags.flat_map(&:books).reject { |book| book == @book }.uniq.take(3)
   end
 
   private
