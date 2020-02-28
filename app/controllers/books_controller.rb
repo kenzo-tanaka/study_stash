7049aa32399f0f54e4746f6a4864862d97631f9e
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2020-02-28 16:50:19 +0900

    fix: コメント関連のN+1問題を修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,7 @@ class BooksController < ApplicationController
   # GET /books/1
   # GET /books/1.json
   def show
-    @comments = @book.comments
+    @comments = @book.comments.includes(:user)
   end
 
   private
