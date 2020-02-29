e71d1c6b99004a5e1527578bfb1709ea79b69183
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2020-02-29 09:51:23 +0900

    feat: userとblogを関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,7 @@ class User < ApplicationRecord
 
   has_many :schedules, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_one :blog
 
   scope :most_studies_users, lambda {
     joins(:schedules)
