ebe46bb22bdb697429aa1f85a3626e01bccb7a16
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2020-02-29 21:10:05 +0900

    test: articles_specを修正

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Articles", type: :request do
   let(:user) { create(:user) }
-  let(:blog) { create(:blog, user: user) }
+  let!(:blog) { create(:blog, user: user) }
   let(:article) { create(:article, blog: blog) }
 
   before { sign_in user }
