6696dcda516de74275f97fb1bd65fd080c69ad2d
kenzo <kenzou.kenzou104809@gmail.com>
2020-05-26 21:27:26 +0900

    fix: 自作済のadmin/下のcontrollerを削除

--- app/controllers/admin/application_controller.rb
+++
@@ -1,7 +0,0 @@
-class Admin::ApplicationController < ApplicationController
-  before_action :check_admin_user
-  private
-  def check_admin_user
-    redirect_to root_path unless current_user&.admin?
-  end
-end
